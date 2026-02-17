67c8512cf79e6770033c9ac13a9c390baf9bd54a
cforcliffs <cforcliffs@gmail.com>
2026-02-17 19:00:20 +0000

    fixed the padding size

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
+
+// Variables for the JavaScript to function
+const genBtn = document.getElementById("genbtn-el")
 let passwordOne = document.getElementById("password1-el")
 let passwordTwo = document.getElementById("password2-el")
 
 
-
+// Characters and the password length
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
@@ -11,8 +14,7 @@ let passwordLength = 14;
 
 
 
-
-
+// How to get 14 random characters
 function getRandomCharacter(){
 
     let randomCharacter = Math.floor(Math.random() * characters.length)
@@ -23,6 +25,8 @@ function getRandomCharacter(){
 
 
 
+
+// Password one and password two use this formula to get random passwords
 function generateRandomPassword(){
     let randomPassword = ""
     for(let i = 0; i < passwordLength; i++){
@@ -31,8 +35,14 @@ function generateRandomPassword(){
     return randomPassword
 }
 
-// Function to generate and display both passwords
-function generatePasswords(){
+
+
+
+
+// When the user clicks the green button, it should trigger this event liste
+
+genBtn.addEventListener('click', function(){
+
     // Generate two different passwords
     let password1 = generateRandomPassword()
     let password2 = generateRandomPassword()
@@ -40,9 +50,8 @@ function generatePasswords(){
     // Display the passwords in the HTML elements
     passwordOne.innerText = password1
     passwordTwo.innerText = password2
-}
 
-
+})
 
 
 
